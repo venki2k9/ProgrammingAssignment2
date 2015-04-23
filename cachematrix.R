ef8f39cd21d30096eb560c1561224fa6b050601f
venki2k9 <shyam.tumuluri@gmail.com>
2015-04-22 19:23:49 -0500

    Programming assignment is completed
    CacheMatrix and cacheSolve are completed and tested. Comments are put in
    the code as well

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and CacheSolve are functions used to store
+## and create inverses of square matrix which does not get modified
 
-## Write a short comment describing this function
+## makeCacheMatrix is used to create a list which caches the input
+## matrix as well as its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  t <- NULL
+  set <- function(y){
+    x <<- y
+    t <<- NULL
+    
+  }
+  
+  get <- function() x
+  setsolve <- function(xt) t <<- xt
+  getsolve <- function() t  
+  list(set = set, get = get, getsolve = getsolve, setsolve = setsolve)  
 }
 
 
-## Write a short comment describing this function
+## cachesolve checks if the Inverse of the matrix inverse already exists.
+## in the Cache. If not, it would create the Inverse and Cache it
+## for future use.
 
 cacheSolve <- function(x, ...) {
+  Inv <- x$getsolve()
+  if(!is.null(Inv)){
+    message("getting cached inverse matrix")
+    return(Inv)    
+  }
+  m <- x$get()
+  Inv <- solve(m)
+  x$setsolve(Inv)
+  Inv
         ## Return a matrix that is the inverse of 'x'
 }
+
